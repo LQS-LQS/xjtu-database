0145625c067c5f71fe9406a86d24683c2da5dba1
lqs-global <fuck-global@qq.com>
2022-09-24 19:42:44 +0800

    feat:格式化js

--- 获得课程信息&学生.js
+++ 获得课程信息&学生.js
@@ -18,15 +18,15 @@ const Cookie = 'EMAP_LANG=zh; THEME=cherry; _WEU=K8d4YPaxIg3wO0XViBBzGjR5CGSGpWs
 let randomNumer = 6;
 let COURSE_DEPARTEMT = new Map();
 COURSE_DEPARTEMT.set('人文社会科学学院', 'RW').set('医学部', 'YX').set('电气工程学院', 'DQ')
-                .set('马克思主义学院', 'MK').set('机械工程学院', 'JX').set('化学学院', 'HX')
-                .set('法学院', 'FX').set('生命科学与技术学院', 'SM').set('经济与金融学院', 'JJ')
-                .set('物理学院', 'WL').set('管理学院', 'GL').set('人居环境与建筑工程学院', 'RJ')
-                .set('外国语学院', 'WG').set('公共政策与管理学院', 'GG').set('体育中心', 'TY')
-                .set('能源与动力工程学院', 'ND').set('金禾经济研究中心', 'JH').set('化学工程与技术学院', 'HX')
-                .set('数学与统计学院', 'SX').set('实践教学中心/工程坊', 'SB').set('航天航空学院', 'HK')
-                .set('军事教研室', 'JS').set('材料科学与工程学院', 'CL').set('国际教育学院', 'GJ')
-                .set('新闻与新媒体学院', 'XW').set('钱学森学院/钱学森书院', 'QX').set('学工部/学生处/武装部', 'XG')
-                .set('前沿科学技术研究院', 'QY').set('学生就业创业指导服务中心', 'JY').set('教务处', 'JW');
+  .set('马克思主义学院', 'MK').set('机械工程学院', 'JX').set('化学学院', 'HX')
+  .set('法学院', 'FX').set('生命科学与技术学院', 'SM').set('经济与金融学院', 'JJ')
+  .set('物理学院', 'WL').set('管理学院', 'GL').set('人居环境与建筑工程学院', 'RJ')
+  .set('外国语学院', 'WG').set('公共政策与管理学院', 'GG').set('体育中心', 'TY')
+  .set('能源与动力工程学院', 'ND').set('金禾经济研究中心', 'JH').set('化学工程与技术学院', 'HX')
+  .set('数学与统计学院', 'SX').set('实践教学中心/工程坊', 'SB').set('航天航空学院', 'HK')
+  .set('军事教研室', 'JS').set('材料科学与工程学院', 'CL').set('国际教育学院', 'GJ')
+  .set('新闻与新媒体学院', 'XW').set('钱学森学院/钱学森书院', 'QX').set('学工部/学生处/武装部', 'XG')
+  .set('前沿科学技术研究院', 'QY').set('学生就业创业指导服务中心', 'JY').set('教务处', 'JW');
 
 let COURSE_NAME = [];
 let COURSE_NUM = [];
@@ -56,7 +56,7 @@ async function sendPostToGetInfo() {
   const body = await res.json();
   console.log(body);
   let courseArray = body.datas.qxfbkccx.rows;
-  
+
 
   //参数的意思
   /**
@@ -75,16 +75,16 @@ async function sendPostToGetInfo() {
     //判断为null  undefined 包含空格(说明这是外教)  
     if (!(item.SKJS == null || item.SKJS == undefined || item.SKJS.includes(' ')))
       STUDENT_NAME = STUDENT_NAME.concat(item.SKJS + ',')
-      
+
     let cno = COURSE_DEPARTEMT.get(item.KKDWDM_DISPLAY);
     if (cno == undefined) {//确定是EE还是CS
       //CS的课
-      if ((item.KCM != null && item.KCM.indexOf('计算机') != -1) || (item.SKBJ != null && item.SKBJ.indexOf('计算机') != -1))   cno = 'CS';
-      else  cno = 'EE';
+      if ((item.KCM != null && item.KCM.indexOf('计算机') != -1) || (item.SKBJ != null && item.SKBJ.indexOf('计算机') != -1)) cno = 'CS';
+      else cno = 'EE';
     }
 
     //之前没有找到并且课程名字不包括（
-    if (COURSE_NAME.includes(item.KCM) == false && item.KCM.includes('（')==false) {
+    if (COURSE_NAME.includes(item.KCM) == false && item.KCM.includes('（') == false) {
       COURSE_NAME.push(item.KCM.slice(0, 18));
       COURSE_NUM.push(cno + '-' + randomNumer);
       randomNumer++;
@@ -126,13 +126,13 @@ await sendPostToGetInfo();
 
 console.log(COURSE_NAME);
 //把课程写到文件里面去
-for(let i=0;i<COURSE_NUM.length;i++){
+for (let i = 0; i < COURSE_NUM.length; i++) {
   let cname = COURSE_NAME[i];
   let cno = COURSE_NUM[i];
 
-  for(let j=0;j<cname.length;j++) writeStream_COURSE.write(cname[j]);
+  for (let j = 0; j < cname.length; j++) writeStream_COURSE.write(cname[j]);
   writeStream_COURSE.write('#$');
-  for(let j=0;j<cno.length;j++) writeStream_COURSE.write(cno[j]);
+  for (let j = 0; j < cno.length; j++) writeStream_COURSE.write(cno[j]);
   writeStream_COURSE.write('\n');
 }
 
@@ -145,21 +145,21 @@ STUDENT_NAME_ARRAY = [...tmpSet];
 
 //随机生成不同的名字
 let initLength = STUDENT_NAME_ARRAY.length;
-for(let i=0;i<initLength;i++){
-  let ranNum = Math.round( Math.random()*(familyName.length-1) );
-  STUDENT_NAME_ARRAY.push( familyName[ranNum] + STUDENT_NAME_ARRAY[i].slice(1) )
-  ranNum = Math.round( Math.random()*(familyName.length-1) );
-  STUDENT_NAME_ARRAY.push( familyName[ranNum] + STUDENT_NAME_ARRAY[i].slice(1) )
+for (let i = 0; i < initLength; i++) {
+  let ranNum = Math.round(Math.random() * (familyName.length - 1));
+  STUDENT_NAME_ARRAY.push(familyName[ranNum] + STUDENT_NAME_ARRAY[i].slice(1))
+  ranNum = Math.round(Math.random() * (familyName.length - 1));
+  STUDENT_NAME_ARRAY.push(familyName[ranNum] + STUDENT_NAME_ARRAY[i].slice(1))
 }
-STUDENT_NAME_ARRAY = STUDENT_NAME_ARRAY.sort((a,b)=>Math.random()-0.5);
+STUDENT_NAME_ARRAY = STUDENT_NAME_ARRAY.sort((a, b) => Math.random() - 0.5);
 
 let STUDENT_NAME_ARRAY_UNIQUE = new Set(STUDENT_NAME_ARRAY)
 
 let fuck = [...STUDENT_NAME_ARRAY_UNIQUE]
 let ssstr = fuck.join(',');
 
-for(let i=0;i<ssstr.length;i++){
+for (let i = 0; i < ssstr.length; i++) {
   let item = ssstr[i];
-  if(item==',') writeStream_Student.write('\n');
+  if (item == ',') writeStream_Student.write('\n');
   else writeStream_Student.write(item);
 }
